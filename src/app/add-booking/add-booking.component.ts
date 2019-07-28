5b3d8dc86616ee19ea8443ff9af8a0499f9a0077
max <max@arch.mshome.net>
2019-07-28 19:57:57 +0200

    working local

--- src/app/add-booking/add-booking.component.ts
+++ src/app/add-booking/add-booking.component.ts
@@ -14,6 +14,7 @@ import {AuthService} from '../auth.service';
 export class AddBookingComponent implements OnInit {
   public apiUrl = environment.apiUrl;
   public data;
+  public p;
   public pickerOptions: any;
   public bookings;
   public ownDates = [];
